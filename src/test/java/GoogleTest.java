d8b87c23a18869395f2260656b65ec1026eb71f6
LoganOn <logan15.krawczyk@gmail.com>
2019-06-09 17:19:51 +0200

    Printed out number of results

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,4 +1,5 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -8,19 +9,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
-
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class GoogleTest {
     private WebDriver driver;
     private WebDriverWait wait;
+
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Konrad\\Downloads\\Selenium_StaplesSolutions\\src\\test\\java\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--lang=pl");
-        driver =  new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, 10);
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
@@ -34,15 +34,32 @@ public class GoogleTest {
         driver.findElement(By.className("gLFyf")).sendKeys("staples solutions uk");
         wait.until(ExpectedConditions.elementToBeClickable(By.className("gNO89b")));
         driver.findElement(By.className("gNO89b")).click();
-
-
-
-
+        wait.until(visibilityOfElementLocated(By.id("resultStats")));
+        String result = driver.findElement(By.id("resultStats")).getText();
+        System.out.println("Number of found : " + cuteResult(result));
     }
 
     @After
     public void tearDown() {
         driver.quit();
+    }
+
+    public int cuteResult(String s) {
+        s.trim();
+        String str = s.substring(0, s.indexOf('w') - 1);
+        String[] strArray = str.split("Około");
+        String[] result = strArray[1].split(" ");
+        int i = Integer.valueOf(concatenateResult(result));
+
+        return i;
+    }
+
+    public String concatenateResult(String[] s) {
+        String concatenate = "";
+        for (String str : s)
+            concatenate = concatenate.concat(str);
+
+        return concatenate;
     }
 
 
