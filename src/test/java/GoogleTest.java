e08aa1554b0cbf690c03b6adbec22fe12c3453f8
LoganOn <logan15.krawczyk@gmail.com>
2019-06-09 16:48:55 +0200

    Searched for a query

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,28 +1,42 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 public class GoogleTest {
     private WebDriver driver;
+    private WebDriverWait wait;
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Konrad\\Downloads\\Selenium_StaplesSolutions\\src\\test\\java\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--lang=us");
-        driver =  new ChromeDriver();
+        options.addArguments("--lang=pl");
+        driver =  new ChromeDriver(options);
+        wait = new WebDriverWait(driver, 10);
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
-        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+
 
     }
 
     @Test
     public void googleSearchTest() {
+        wait.until(visibilityOfElementLocated(By.className("gLFyf")));
+        driver.findElement(By.className("gLFyf")).sendKeys("staples solutions uk");
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("gNO89b")));
+        driver.findElement(By.className("gNO89b")).click();
+
+
+
 
     }
 
